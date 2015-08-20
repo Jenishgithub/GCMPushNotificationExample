a98115753999416363567cd3dca75b4be33e0bb8
Jenishgithub <jenish_shakya33@yahoo.com>
2015-08-20 08:55:31 -0800

    Initial commit

---
+++ src/com/prgguru/example/ApplicationConstants.java
@@ -0,0 +1,14 @@
+package com.prgguru.example;
+
+public interface ApplicationConstants {
+
+	// Php Application URL to store Reg ID created
+	static final String APP_SERVER_URL = "http://localhost/gcm/gcm.php?shareRegId=true";
+	// static final String APP_SERVER_URL = "http://localhost/gcm/gcm.php";
+
+	// Google Project Number
+	static final String GOOGLE_PROJ_ID = "537875107274";
+	// Message Key
+	static final String MSG_KEY = "m";
+
+}
